9ae6a93624c6d547c33fd82de86f650a28e9ac16
Dong-Young Kim <31337.persona@gmail.com>
2022-04-27 19:58:59 +0900

    feat: add user module

---
+++ server/src/users/controllers/users.controller.ts
@@ -0,0 +1,44 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  Patch,
+  Post,
+} from "@nestjs/common";
+import { CreateUserDto } from "../dto/create-user.dto";
+import { UpdateUserDto } from "../dto/update-user.dto";
+import { UsersService } from "../services/users.service";
+
+@Controller("users")
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  async create(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.create(createUserDto);
+  }
+
+  @Get()
+  async findAll() {
+    return this.usersService.findAll();
+  }
+
+  @Get(":id")
+  async findOne(@Param("id") id: string) {
+    return this.usersService.findOne(+id);
+  }
+
+  @Patch(":id")
+  async update(@Param("id") id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.usersService.update(+id, updateUserDto);
+  }
+
+  @Delete(":id")
+  @HttpCode(204)
+  async remove(@Param("id") id: string) {
+    return this.usersService.remove(+id);
+  }
+}

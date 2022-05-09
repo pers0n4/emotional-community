b679346aa169f6c15054d41c29c55ccda87b0cd6
Dong-Young Kim <31337.persona@gmail.com>
2022-05-10 01:16:42 +0900

    test: update tests
    - remove `auth.service` test
    - add mock repository to `users.controller` test
    - add mock repository to `users.service` test

--- server/src/users/controllers/users.controller.spec.ts
+++ server/src/users/controllers/users.controller.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from "@nestjs/testing";
+import { getRepositoryToken } from "@nestjs/typeorm";
 
+import { User } from "../entities/user.entity";
 import { UsersService } from "../services/users.service";
 
 import { UsersController } from "./users.controller";
@@ -10,7 +12,19 @@ describe("UsersController", () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: {
+            create: jest.fn(),
+            findAll: jest.fn(),
+            findOne: jest.fn(),
+            update: jest.fn(),
+            delete: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
--- server/src/users/services/users.service.spec.ts
+++ server/src/users/services/users.service.spec.ts
@@ -1,4 +1,7 @@
 import { Test, TestingModule } from "@nestjs/testing";
+import { getRepositoryToken } from "@nestjs/typeorm";
+
+import { User } from "../entities/user.entity";
 
 import { UsersService } from "./users.service";
 
@@ -7,7 +10,19 @@ describe("UsersService", () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: {
+            create: jest.fn(),
+            findAll: jest.fn(),
+            findOne: jest.fn(),
+            update: jest.fn(),
+            delete: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);

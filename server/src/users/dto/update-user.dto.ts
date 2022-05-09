97b6e4731cd07afa2862a655e60c83c138e7548a
Dong-Young Kim <31337.persona@gmail.com>
2022-05-09 11:40:17 +0900

    feat: add validation rules to user dtos

--- server/src/users/dto/update-user.dto.ts
+++ server/src/users/dto/update-user.dto.ts
@@ -1,7 +1,7 @@
-import { PartialType } from "@nestjs/mapped-types";
+import { OmitType, PartialType } from "@nestjs/mapped-types";
 
 import { CreateUserDto } from "./create-user.dto";
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {
-  readonly password: string;
-}
+export class UpdateUserDto extends PartialType(
+  OmitType(CreateUserDto, ["username"] as const),
+) {}

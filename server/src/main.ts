9c414b1de2875c16b97f33722debbd48c300b013
Dong-Young Kim <31337.persona@gmail.com>
2022-06-05 03:06:40 +0900

    perf: clean unneeded from main.ts

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,4 +1,3 @@
-import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import {
   FastifyAdapter,
@@ -10,7 +9,7 @@ import { AppModule } from "./app.module";
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    new FastifyAdapter({ logger: true }),
+    new FastifyAdapter(),
   );
 
   app.enableCors();

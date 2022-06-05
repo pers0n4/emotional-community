761aae5d29bc86e329b8d271674fa421078e74de
Dong-Young Kim <31337.persona@gmail.com>
2022-06-06 04:01:45 +0900

    feat: add tracks page

---
+++ client/pages/tracks/_id.vue
@@ -0,0 +1,26 @@
+<template>
+  <main class="p-4">
+    <h2 class="is-size-2">{{ id }}</h2>
+    <ul>
+      <li v-for="comment in comments" :key="comment.id">
+        {{ comment.body }}
+      </li>
+    </ul>
+  </main>
+</template>
+
+<script>
+  export default {
+    async asyncData({ $axios, params }) {
+      const { id } = params;
+
+      const comments = await $axios.$get(`/comments`, {
+        params: {
+          track: id,
+        },
+      });
+
+      return { id, comments };
+    },
+  };
+</script>

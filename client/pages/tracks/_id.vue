dfd0cfe9b55c3bb8cc93a758ba14bdcab619d61d
Dong-Young Kim <31337.persona@gmail.com>
2022-06-06 23:16:18 +0900

    feat: add charts to track page

--- client/pages/tracks/_id.vue
+++ client/pages/tracks/_id.vue
@@ -1,6 +1,10 @@
 <template>
   <main class="p-4">
-    <h2 class="title">{{ track }}</h2>
+    <h2 class="title">{{ title }} - {{ artist }}</h2>
+    <section class="is-flex">
+      <v-chart class="chart" :option="sentimentChart" />
+      <v-chart class="chart" :option="wordCloud" />
+    </section>
     <ul>
       <li v-for="comment in comments" :key="comment.id">
         {{ comment.body }}
@@ -10,17 +14,112 @@
 </template>
 
 <script>
+  import { PieChart } from "echarts/charts";
+  import {
+    LegendComponent,
+    TitleComponent,
+    TooltipComponent,
+  } from "echarts/components";
+  import { use } from "echarts/core";
+  import theme from "echarts/lib/theme/light";
+  import { CanvasRenderer } from "echarts/renderers";
+  import VChart from "vue-echarts";
+
+  use([
+    CanvasRenderer,
+    PieChart,
+    TitleComponent,
+    TooltipComponent,
+    LegendComponent,
+  ]);
+
   export default {
+    components: {
+      VChart,
+    },
     async asyncData({ $axios, params }) {
-      const { id, track } = params;
+      const { id } = params;
 
-      const comments = await $axios.$get(`/comments`, {
-        params: {
-          track: id,
+      const { title, artist, comments, sentiments, entities } =
+        await $axios.$get(`/tracks/${id}`);
+
+      return {
+        id,
+        title,
+        artist,
+        comments,
+        sentiments,
+        entities,
+      };
+    },
+    data() {
+      return {
+        sentimentChart: null,
+        wordCloud: null,
+      };
+    },
+    created() {
+      this.sentimentChart = {
+        tooltip: {
+          trigger: "item",
         },
-      });
+        legend: {
+          orient: "vertical",
+          left: "left",
+        },
+        series: [
+          {
+            name: "Sentiment",
+            type: "pie",
+            radius: "50%",
+            data: Object.entries(this.sentiments).map(([key, value]) => ({
+              value,
+              name: key,
+            })),
+          },
+        ],
+      };
 
-      return { id, track, comments };
+      this.wordCloud = {
+        series: [
+          {
+            type: "wordCloud",
+            // shape: "circle",
+            // keepAspect: false,
+            // left: "center",
+            // top: "center",
+            // width: "70%",
+            // height: "80%",
+            // right: null,
+            // bottom: null,
+            // sizeRange: [12, 60],
+            // rotationRange: [-90, 90],
+            // rotationStep: 45,
+            // gridSize: 8,
+            // drawOutOfBound: false,
+            // layoutAnimation: true,
+            textStyle: {
+              fontFamily: "sans-serif",
+              fontWeight: "bold",
+              color() {
+                return theme.color[
+                  Math.floor(Math.random() * theme.color.length)
+                ];
+              },
+            },
+            data: Object.entries(this.entities).map(([key, value]) => ({
+              name: key,
+              value,
+            })),
+          },
+        ],
+      };
     },
   };
 </script>
+
+<style scoped>
+  .chart {
+    height: 300px;
+  }
+</style>
